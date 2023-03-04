6d25f8f8ec947474e3d367615d19adeebcb9d215
miguelriverasarmiento <miguelriverasarmiento@gmail.com>
2023-03-04 11:01:22 -0500

    Started with form react

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import './App.css'
+import { useState } from 'react';
 
 function App() {
+
+  const [post, setPost] = useState({
+    title: '',
+    photo: null
+  });
 
   return (
     <div>
       <h1>Node and React</h1>
       <form>
-        <input type="text" placeholder="title" />
-        <input type="file" name='photo' onChange={(e) => console.log(e)}/>
+        <input type="text" placeholder="title" onChange={e => setPost({...post, title: e.target.value})}/>
+        <input type="file" name='photo' onChange={(e) => setPost({...post, photo: e.target.files[0]})}/>
         <button>
           Subir
         </button>
