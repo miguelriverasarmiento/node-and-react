0bda93fbb51f63af3f238c26d4dda662a7d4c110
miguelriverasarmiento <miguelriverasarmiento@gmail.com>
2023-03-06 17:48:39 -0500

    Frontend finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
 
@@ -7,12 +8,29 @@ function App() {
     photo: null
   });
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('photo', post.photo)
+    
+    const response = await axios.post('http://localhost:3000/upload', formData, {
+      headers: {
+        "Content-Type": "multipart/form-data"
+      }
+    })
+
+    return console.log(response)
+  }
+
   return (
     <div>
       <h1>Node and React</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
+
         <input type="text" placeholder="title" onChange={e => setPost({...post, title: e.target.value})}/>
+
         <input type="file" name='photo' onChange={(e) => setPost({...post, photo: e.target.files[0]})}/>
+        
         <button>
           Subir
         </button>
